633354e4cba4f137bc74ddc15903142bb2b419cc
Dony-Ahmad-Hisyam <donyahmad4321@gmail.com>
2024-02-22 09:55:08 +0700

    04

--- expresbasic/routes/kategori.js
+++ expresbasic/routes/kategori.js
@@ -16,5 +16,34 @@ router.get("/", function (req, res, next) {
     }
   );
 });
+router.get("/create", function (req, res, next) {
+  res.render("kategori/create", {
+    nama_kategori: "",
+  });
+});
 
+router.post("/store", function (req, res, next) {
+  try {
+    let { nama_kategori } = req.body;
+    let data = {
+      nama_kategori: nama_kategori,
+    };
+    connection.query(
+      "INSERT INTO kategori SET ?",
+      data,
+      function (err, result) {
+        if (err) {
+          req.flash("error", "Gagal menyimpan data!");
+          res.redirect("/kategori");
+        } else {
+          req.flash("success", "Berhasil menyimpan data!");
+          res.redirect("/kategori");
+        }
+      }
+    );
+  } catch (error) {
+    req.flash("error", "Terjadi kesalahan pada fungsi");
+    res.redirect("/kategori");
+  }
+});
 module.exports = router;
